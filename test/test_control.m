6dfaef182797ecf4e774fe1b5ddfddd239bb4ec6
Vegard Nitter Vestad <vegard@vnvestad.com>
2019-03-10 01:42:19 +0100

    Add support for storing GNC settings in file

--- test/test_control.m
+++ test/test_control.m
@@ -1,4 +1,7 @@
+clear
+
 ship = prob.ship.load('ship_viknes830.json');
+gnc_settings = jsondecode(fileread('viknes830.gnc.json'));
 
 % Conditions
 xx0 = [0 0 pi 0 0 0]';
@@ -6,13 +9,8 @@ xx0 = [0 0 pi 0 0 0]';
 psi_d = pi/2;
 u_d = 5;
 
-settings.heading.K_p = 500;
-settings.heading.K_d = 5000;
-settings.surge.K_p = 3000;
-settings.surge.K_i = 300;
-
 %% Simulate
-controller = @(xx,aux) prob.gnc.control.surge_heading(xx(3,:), xx(4,:), xx(6,:), psi_d, u_d, aux, ship, settings);
+controller = @(xx,aux) prob.gnc.control.surge_heading(xx(3,:), xx(4,:), xx(6,:), psi_d, u_d, aux, ship, gnc_settings.control);
 
 trajectory = ship.simulate(xx0, 0, controller, 100, []); %@(x,T) controller.calculate(x,T,u_d,psi_d)
 
--- test/test_gnc.m
+++ test/test_gnc.m
@@ -1,6 +1,7 @@
 clear all
 
-ship = prob.ship.load('ship_viknes830.json');
+ship = prob.ship.load('viknes830.ship.json');
+gnc_settings = jsondecode(fileread('viknes830.gnc.json'));
 
 % Conditions
 path = [0 250 600 800 500 100 450;
@@ -10,14 +11,9 @@ u_d = 5;
 xx0 = [0 0 0 0 0 0]';
 aux0 = [1;0];
 
-settings.control.heading.K_p = 500;
-settings.control.heading.K_d = 5000;
-settings.control.surge.K_p = 3000;
-settings.control.surge.K_i = 300;
-settings.guidance.R = 100;
-settings.guidance.Delta = 60;
 
-gnc = @(xx,aux) prob.gnc.complete(xx, aux, path, u_d, ship, settings);
+
+gnc = @(xx,aux) prob.gnc.complete(xx, aux, path, u_d, ship, gnc_settings);
 
 
 % Simulate
